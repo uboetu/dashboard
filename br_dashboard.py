8757dc928b0c9c7dd74037742ff7aeb77a703787
uboetu <boetrijnders@gmail.com>
2023-09-23 16:01:55 +0200

    Initial commit

---
+++ br_dashboard.py
@@ -0,0 +1,49 @@
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# Load the data
+@st.cache
+def load_data():
+    data = pd.read_csv(r"C:\Users\boetr\Desktop\Programeren\brazil traffic\Dados_PRF_2022_translated.csv", delimiter=';')
+    return data
+
+df = load_data()
+
+# Title and introduction
+st.title("Brazil Traffic Data Dashboard for 2022")
+st.write("This dashboard provides insights into the traffic data of Brazil for the year 2022.")
+
+# Data Overview
+st.subheader('Data Overview')
+st.write(f"Total Accidents: {len(df)}")
+st.write(f"Total Deceased: {df['deceased'].sum()}")
+st.write(f"Total Injured: {df['injured'].sum()}")
+
+# Filtering
+state = st.multiselect("Select State(s)", df['state'].unique())
+if state:
+    df = df[df['state'].isin(state)]
+
+# Display raw data on request
+if st.checkbox('Show raw data'):
+    st.subheader('Raw data')
+    st.write(df)
+
+# Visualizations
+st.subheader('Number of Accidents per State')
+fig, ax = plt.subplots()
+df['state'].value_counts().plot(kind='bar', ax=ax)
+st.pyplot(fig)
+
+st.subheader('Distribution of Accidents based on Accident Cause')
+fig, ax = plt.subplots()
+df['accident_cause'].value_counts().plot(kind='bar', ax=ax)
+st.pyplot(fig)
+
+st.subheader('Distribution of Accidents based on Weekday')
+fig, ax = plt.subplots()
+df['weekday'].value_counts().plot(kind='bar', ax=ax)
+st.pyplot(fig)
+
+# Add more visualizations and interactivity...
---
+++ br_traffic_v2_translated_adaptable.py
@@ -0,0 +1,234 @@
+
+import pandas as pd
+
+import os
+script_dir = os.path.dirname(os.path.abspath(__file__))
+file_path_template = os.path.join(script_dir, 'Dados_PRF_{}.csv')
+dfs = []
+
+# Dictionary for column name translations
+translation_dict = {
+    'id': 'ID',
+    'data_inversa': 'date',
+    'dia_semana': 'weekday',
+    'horario': 'time',
+    'uf': 'state',
+    'br': 'highway',
+    'km': 'kilometer',
+    'municipio': 'city',
+    'causa_acidente': 'accident_cause',
+    'tipo_acidente': 'accident_type',
+    'classificacao_acidente': 'accident_classification',
+    'fase_dia': 'time_of_day',
+    'sentido_via': 'direction',
+    'condicao_metereologica': 'weather_condition',
+    'tipo_pista': 'type_of_road',
+    'tracado_via': 'road_layout',
+    'uso_solo': 'urban_rural',
+    'ano': 'year',
+    'pessoas': 'people',
+    'mortos': 'deceased',
+    'feridos_leves': 'lightly_injured',
+    'feridos_graves': 'severely_injured',
+    'ilesos': 'unharmed',
+    'ignorados': 'unknown',
+    'feridos': 'injured',
+    'veiculos': 'vehicles',
+    'latitude': 'latitude',
+    'longitude': 'longitude',
+    'regional': 'regional',
+    'delegacia': 'police_station',
+    'uop': 'operational_unit'
+}
+
+
+# Dictionary for specific value translations
+translations = {
+    "weekday": {
+        'sábado': 'Saturday',
+        'domingo': 'Sunday',
+        'segunda-feira': 'Monday',
+        'terça-feira': 'Tuesday',
+        'quarta-feira': 'Wednesday',
+        'quinta-feira': 'Thursday',
+        'sexta-feira': 'Friday'
+    },
+    "accident_cause": {
+        'Ingestão de álcool pelo condutor': 'Driver alcohol consumption',
+        'Condutor deixou de manter distância do veículo da frente': 'Driver failed to maintain distance from the vehicle in front',
+        'Reação tardia ou ineficiente do condutor': 'Late or inefficient driver reaction',
+        'Acumulo de água sobre o pavimento': 'Water accumulation on the pavement',
+        'Mal súbito do condutor': 'Driver sudden illness',
+        'Chuva': 'Rain',
+        'Ausência de reação do condutor': 'Lack of driver reaction',
+        'Manobra de mudança de faixa': 'Lane change maneuver',
+        'Pista Escorregadia': 'Slippery road',
+        'Ausência de sinalização': 'Lack of signaling',
+        'Condutor Dormindo': 'Driver sleeping',
+        'Velocidade Incompatível': 'Incompatible speed',
+        'Acessar a via sem observar a presença dos outros veículos': 'Entering the roadway without noticing other vehicles',
+        'Conversão proibida': 'Forbidden conversion',
+        'Transitar na contramão': 'Driving on the wrong side',
+        'Ultrapassagem Indevida': 'Improper overtaking',
+        'Desrespeitar a preferência no cruzamento': 'Disregarding intersection priority',
+        'Acostamento em desnível': 'Uneven shoulder',
+        'Demais falhas mecânicas ou elétricas': 'Other mechanical or electrical failures',
+        'Carga excessiva e/ou mal acondicionada': 'Excessive and/or poorly stowed load',
+        'Problema com o freio': 'Brake issue',
+        'Ingestão de álcool e/ou substâncias psicoativas pelo pedestre': 'Pedestrian consumption of alcohol and/or psychoactive substances',
+        'Curva acentuada': 'Sharp curve',
+        'Estacionar ou parar em local proibido': 'Parking or stopping in a prohibited place',
+        'Avarias e/ou desgaste excessivo no pneu': 'Damages and/or excessive tire wear',
+        'Pedestre cruzava a pista fora da faixa': 'Pedestrian crossing the road outside the crosswalk',
+        'Obstrução na via': 'Road obstruction',
+        'Acesso irregular': 'Irregular access',
+        'Pista esburacada': 'Potholed road',
+        'Animais na Pista': 'Animals on the road',
+        'Falta de acostamento': 'Lack of shoulder',
+        'Entrada inopinada do pedestre': 'Unexpected pedestrian entry',
+        'Acumulo de areia ou detritos sobre o pavimento': 'Accumulation of sand or debris on the pavement',
+        'Pedestre andava na pista': 'Pedestrian walking on the road',
+        'Desvio temporário': 'Temporary detour',
+        'Transitar no acostamento': 'Driving on the shoulder',
+        'Problema na suspensão': 'Suspension problem',
+        'Objeto estático sobre o leito carroçável': 'Static object on the carriageway',
+        'Deficiência do Sistema de Iluminação/Sinalização': 'Deficiency in the Lighting/Signaling System',
+        'Trafegar com motocicleta (ou similar) entre as faixas': 'Riding a motorcycle (or similar) between lanes',
+        'Condutor usando celular': 'Driver using cellphone',
+        'Restrição de visibilidade em curvas horizontais': 'Restricted visibility on horizontal curves',
+        'Ingestão de álcool ou de substâncias psicoativas pelo pedestre': 'Pedestrian consumption of alcohol or psychoactive substances',
+        'Declive acentuado': 'Steep decline',
+        'Frear bruscamente': 'Braking sharply',
+        'Iluminação deficiente': 'Poor lighting',
+        'Área urbana sem a presença de local apropriado para a travessia de pedestres': 'Urban area without an appropriate pedestrian crossing location',
+        'Demais Fenômenos da natureza': 'Other natural phenomena',
+        'Demais falhas na via': 'Other roadway failures',
+        'Faixas de trânsito com largura insuficiente': 'Traffic lanes with insufficient width',
+        'Obras na pista': 'Roadworks',
+        'Retorno proibido': 'Forbidden U-turn',
+        'Afundamento ou ondulação no pavimento': 'Pavement sinking or undulation',
+        'Falta de elemento de contenção que evite a saída do leito carroçável': 'Lack of containment element preventing the exit from the carriageway',
+        'Ingestão de substâncias psicoativas pelo condutor': 'Driver consumption of psychoactive substances',
+        'Neblina': 'Fog',
+        'Condutor desrespeitou a iluminação vermelha do semáforo': 'Driver disrespected the red traffic light',
+        'Sistema de drenagem ineficiente': 'Inefficient drainage system',
+        'Acumulo de óleo sobre o pavimento': 'Oil accumulation on the pavement',
+        'Sinalização mal posicionada': 'Poorly positioned signage',
+        'Pista em desnível': 'Uneven road',
+        'Transitar na calçada': 'Driving on the sidewalk',
+        'Fumaça': 'Smoke',
+        'Redutor de velocidade em desacordo': 'Non-compliant speed reducer',
+        'Deixar de acionar o farol da motocicleta (ou similar)': 'Failure to turn on motorcycle (or similar) headlights',
+        'Restrição de visibilidade em curvas verticais': 'Restricted visibility on vertical curves',
+        'Semáforo com defeito': 'Malfunctioning traffic light',
+        'Faróis desregulados': 'Misaligned headlights',
+        'Modificação proibida': 'Forbidden modification',
+        'Participar de racha': 'Engaging in street racing',
+        'Sinalização encoberta': 'Covered signage'
+    },
+    "accident_type": {
+        'Colisão traseira': 'Rear-end collision',
+        'Tombamento': 'Tipping over',
+        'Colisão frontal': 'Head-on collision',
+        'Saída de leito carroçável': 'Exit from carriageway',
+        'Colisão com objeto': 'Collision with object',
+        'Colisão lateral mesmo sentido': 'Side collision, same direction',
+        'Engavetamento': 'Pile-up',
+        'Atropelamento de pessoa': 'Pedestrian run-over',
+        'Colisão com objeto em movimento': 'Collision with moving object',
+        'Colisão lateral sentido oposto': 'Side collision, opposite direction',
+        'Incêndio': 'Fire',
+        'Queda de motocicleta / bicicleta / veículo': 'Fall from motorcycle/bicycle/vehicle',
+        'Derramamento de carga': 'Cargo spill',
+        'Atropelamento de Animal': 'Animal run-over',
+        'Colisão transversal': 'Transverse collision',
+        'Capotamento': 'Rollover',
+        'Queda de ocupante de veículo': 'Fall of vehicle occupant',
+        'Atropelamento de Pedestre': 'Pedestrian Hit',
+        'Eventos atípicos': 'Atypical events'
+    },
+    "accident_classification": {
+        'Com Vítimas Feridas': 'With Injured Victims',
+        'Com Vítimas Fatais': 'With Fatalities',
+        'Sem Vítimas': 'No Victims'
+    },
+    "time_of_day": {
+        'Plena Noite': 'Deep Night',
+        'Pleno dia': 'Broad Daylight',
+        'Amanhecer': 'Dawn',
+        'Anoitecer': 'Dusk'
+    },
+    "direction": {
+        'Decrescente': 'Decreasing',
+        'Crescente': 'Increasing',
+        'Não Informado': 'Not Informed'
+    },
+    "weather_condition": {
+        'Nublado': 'Cloudy',
+        'Céu Claro': 'Clear Sky',
+        'Chuva': 'Rain',
+        'Sol': 'Sun',
+        'Vento': 'Wind',
+        'Granizo': 'Hail',
+        'Nevoeiro/neblina': 'Fog/Mist',
+        'Neve': 'Snow',
+        'Garoa/Chuvisco': 'Drizzle',
+        'Ignorado': 'Unknown',
+        'Nevoeiro/Neblina': 'Fog/Mist'
+    },
+    "type_of_road": {
+        'Simples': 'Single',
+        'Dupla': 'Double',
+        'Múltipla': 'Multiple'
+    },
+    "road_layout": {
+        'Reta': 'Straight',
+        'Curva': 'Curve',
+        'Desvio temporário': 'Temporary detour',
+        'Rotatória': 'Roundabout',
+        'Ponte': 'Bridge',
+        'Interseção de vias': 'Road intersection',
+        'Retorno regulamentado': 'Regulated return',
+        'Viaduto': 'Overpass',
+        'Túnel': 'Tunnel',
+        'Não Informado': 'Not Informed',
+        'Desvio Temporário': 'Temporary Detour',
+        'Retorno Regulamentado': 'Regulated Return'
+    },
+    "urban_rural": {
+        'Sim': 'Yes',
+        'Não': 'No'
+    }
+}
+
+
+def translate_dataframe(df, translation_dict):
+    """Load the CSV file from the given path, translate its columns, and return the translated dataframe."""
+    df_translated = df.rename(columns=translation_dict)
+    return df_translated
+
+def translate_values(df, translations):
+    for column, mapping in translations.items():
+        if column in df:
+            df[column] = df[column].map(mapping)
+    return df
+
+for year in range(2017, 2024):
+    file_path = file_path_template.format(year)
+    try:
+        df_year = pd.read_csv(file_path, encoding='ISO-8859-1', delimiter=';')
+        df_year = df_year.drop_duplicates()
+        dfs.append(df_year)
+        
+        # Translate column names
+        df_translated = translate_dataframe(df_year, translation_dict)
+        
+        # Translate specific values
+        df_translated = translate_values(df_translated, translations)
+        
+        # Save translated CSV
+        output_file_name = os.path.join(script_dir, "Dados_PRF_" + str(year) + "_translated.csv")
+        df_translated.to_csv(output_file_name, index=False, encoding='utf-8', sep=';')
+        
+    except FileNotFoundError:
+        print("File for year " + str(year) + " not found. Skipping...")
