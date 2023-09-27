70371485a33a157b96d4fcb41b293cdd6e6ded53
uboetu <boetrijnders@gmail.com>
2023-09-27 12:12:22 +0200

    Added coordinate conversion to dataframes

--- br_dashboard.py
+++ br_dashboard.py
@@ -1,105 +1,61 @@
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
-import plotly.graph_objects as go
-import plotly.express as px
+import seaborn as sns
 
-
-# Load the data
+# Load the dataset
 @st.cache
 def load_data():
-    data = pd.read_csv("Dados_PRF_2022_translated.csv", delimiter=';')
+    data = pd.read_csv('Dados_PRF_2022.csv', delimiter=';', encoding='latin1')
     return data
 
 df = load_data()
 
-# Title and introduction
-st.title("Brazil Traffic Data Dashboard for 2022")
-st.write("This dashboard provides insights into the traffic data of Brazil for the year 2022.")
+# Title of the dashboard
+st.title("Accident Data Dashboard")
 
-# Data Overview
-st.subheader('Data Overview')
-st.write(f"Total Accidents: {len(df)}")
-st.write(f"Total Deceased: {df['deceased'].sum()}")
-st.write(f"Total Injured: {df['injured'].sum()}")
+# Selectors for different types of analyses
+analysis = st.sidebar.selectbox("Choose an Analysis Type", 
+                                ["Original Dataset Info", "Time Analysis", "Location Analysis", 
+                                 "Accident Details", "Weather and Road Conditions", "Outcome Analysis"])
 
-# Filtering
-state = st.multiselect("Select State(s)", df['state'].unique())
-if state:
-    df = df[df['state'].isin(state)]
+# Depending on the selected analysis, display the corresponding visualization
+if analysis == "Original Dataset Info":
+    st.subheader("Original Dataset Information")
+    st.write("Delimiter used: ;")
+    st.write("Encoding used: latin1")
+    st.write("Columns in the dataset:")
+    st.write(list(df.columns))
+    st.write("Sample data from the original dataset:")
+    st.write(df.head())
 
-# Display raw data on request
-if st.checkbox('Show raw data'):
-    st.subheader('Raw data')
-    st.write(df)
+elif analysis == "Time Analysis":
+    # Accidents by day of the week
+    st.subheader("Accidents by Day of the Week")
+    fig, ax = plt.subplots()
+    sns.countplot(data=df, y='dia_semana', ax=ax, order=df['dia_semana'].value_counts().index)
+    st.pyplot(fig)
+    
+    # Accidents by time of the day
+    st.subheader("Accidents by Time of the Day")
+    df['horario'] = pd.to_datetime(df['horario'])
+    df['hour'] = df['horario'].dt.hour
+    fig, ax = plt.subplots()
+    sns.histplot(data=df, x='hour', bins=24, ax=ax)
+    st.pyplot(fig)
 
-# Visualizations
-st.subheader('Number of Accidents per State')
-fig, ax = plt.subplots()
-df['state'].value_counts().plot(kind='bar', ax=ax)
-st.pyplot(fig)
+elif analysis == "Location Analysis":
+    # Accidents by federal unit
+    st.subheader("Accidents by Federal Unit")
+    fig, ax = plt.subplots()
+    sns.countplot(data=df, y='uf', ax=ax, order=df['uf'].value_counts().index)
+    st.pyplot(fig)
 
-st.subheader('Distribution of Accidents based on Accident Cause')
-fig, ax = plt.subplots()
-df['accident_cause'].value_counts().plot(kind='bar', ax=ax)
-st.pyplot(fig)
+elif analysis == "Accident Details":
+    # Distribution of the cause of accident
+    st.subheader("Distribution of Cause of Accident")
+    fig, ax = plt.subplots()
+    sns.countplot(data=df, y='causa_acidente', ax=ax, order=df['causa_acidente'].value_counts().index)
+    st.pyplot(fig)
 
-st.subheader('Distribution of Accidents based on Weekday')
-fig, ax = plt.subplots()
-df['weekday'].value_counts().plot(kind='bar', ax=ax)
-st.pyplot(fig)
 
-# Map visualization using Plotly
-# Map the accident types to numeric values
-unique_accident_types = df['accident_type'].unique()
-accident_type_mapping = {accident_type: index for index, accident_type in enumerate(unique_accident_types)}
-df['accident_type_numeric'] = df['accident_type'].map(accident_type_mapping)
-
-# Count the occurrences of each accident type
-accident_type_counts = df['accident_type'].value_counts().reset_index()
-accident_type_counts.columns = ['accident_type', 'count']
-
-# Create a pie chart using Plotly
-fig = px.pie(accident_type_counts, 
-             names='accident_type', 
-             values='count',
-             title='Distribution of Accident Types')
-
-# Show the pie chart in Streamlit
-st.plotly_chart(fig)
-
-# Create a Mapbox scatter plot with markers colored based on mapped accident type values
-fig99 = go.Figure(go.Scattermapbox(
-        lat=df['latitude'],
-        lon=df['longitude'],
-        mode='markers',
-        marker=go.scattermapbox.Marker(
-            size=5,
-            color=df['accident_type_numeric'],
-            colorscale='Viridis',
-            showscale=True,
-            colorbar=dict(tickvals=list(accident_type_mapping.values()), 
-                          ticktext=list(accident_type_mapping.keys()))
-        ),
-        text=df['city'] + '<br>' + df['accident_type'],
-    ))
-
-fig99.update_layout(
-    title='Accidents in Brazil Based on Latitude and Longitude (Colored by Accident Type)',
-    autosize=True,
-    hovermode='closest',
-    showlegend=False,
-    mapbox=go.layout.Mapbox(
-        accesstoken=None,
-        bearing=0,
-        center=go.layout.mapbox.Center(
-            lat=-10,
-            lon=-55
-        ),
-        pitch=0,
-        zoom=3,
-        style='open-street-map'
-    ),
-)
-
-st.plotly_chart(fig99)
