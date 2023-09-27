bb4073e8016e658d0b7a1c85db8649ffcda499e1
uboetu <boetrijnders@gmail.com>
2023-09-27 12:24:31 +0200

    Showing colnames nicer way

--- br_dashboard.py
+++ br_dashboard.py
@@ -38,21 +38,3 @@ if analysis == "Original Dataset Info":
     
     st.write("Sample data from the original dataset:")
     st.write(df.head())
-if analysis == "Original Dataset Info":
-    st.subheader("Original Dataset Information")
-    st.write("Delimiter used: ;")
-    st.write("Encoding used: latin1")
-    
-    # Displaying columns in a more horizontal way
-    st.write("Columns in the dataset:")
-    columns = df.columns.tolist()
-    col1, col2, col3 = st.columns(3)  # create three columns
-    for i in range(0, len(columns), 3):
-        col1.write(f"{i}: `{columns[i]}`")
-        if i + 1 < len(columns):
-            col2.write(f"{i + 1}: `{columns[i + 1]}`")
-        if i + 2 < len(columns):
-            col3.write(f"{i + 2}: `{columns[i + 2]}`")
-    
-    st.write("Sample data from the original dataset:")
-    st.write(df.head())
